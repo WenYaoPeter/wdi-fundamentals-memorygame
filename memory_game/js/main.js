e9cc13e35e5009fba4b74b152f875a09cbf01130
wenyaopeter <peterleewenyao@gmail.com>
2018-05-15 16:46:59 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,23 +7,27 @@ var cardFour = "king";*/
 //console.log("User Flipped" + " " + cardOne);
 //console.log("User Flipped" + " " + cardFour);
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "2king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardsInPlay); //user flipped first queen
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardsInPlay); //user flipped first king
-
-if (cardsInPlay.length === 2) {
-
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
-		alert("Sorry, please try again");
+		alert("Sorry, try again.");
 	}
+};
 
+var flipCard = function (cardId){ //the function when user flips a card
+	
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]); // first queen pushed to cardsInPlay array
+
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
 }
+
+};
+flipCard(0);
+flipCard(1);
